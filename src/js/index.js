cbe38c0c3a52b15add186b9c0d98408bbaba58bc
DayDay <daydaybigking@sina.com>
2020-10-14 08:42:30 +0800

    拆分 css js，添加 dist 文件夹，做好使用 gulp 的准备

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  var wrapper = document.querySelector(".swiper-wrapper");
+  var slideCount = 31;
+  for (var i = 1; i <= slideCount; i++) {
+    var item = document.createElement("div");
+    item.classList.add("swiper-slide");
+    item.style.backgroundImage = "url(./image/" + i + ".jpg)";
+    item.style.backgroundRepeat = "no-repeat";
+    item.style.backgroundSize = "contain";
+    item.style.backgroundPosition = "center";
+    wrapper.appendChild(item);
+  }
+  wrapper.append();
+  var mySwiper = new Swiper(".swiper-container", {
+    loop: true, // 循环轮播
+    effect: "fade", // 设置淡入淡出的变换方式
+    fadeEffect: {
+      crossFade: true,
+    },
+    keyboard: true, // 支持键盘控制
+    autoplay: {
+      delay: 2000, // 自动切换间隔时间，单位毫秒
+      disableOnInteraction: false, // 手动滑动后是否停止自动轮播
+    },
+  });
+
+  // 检测 audio 是否处于自动播放状态
+};
